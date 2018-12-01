7aabd423f7b7a3542cb6570dc897aa1cd221ade9
Ksenia Koldaeva <kseniacold@gmail.com>
2018-12-01 12:45:36 -0800

    Form bolilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './Header/Header';
+import Form from './Form/Form';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <div class="form-wrapper"></div>
+        <Form />
       </div>
     );
   }
