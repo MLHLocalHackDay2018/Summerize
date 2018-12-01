7f3742a8cc70b8762eb6ddd4583a4011105c8e7b
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-12-01 13:07:52 -0800

    send form data to back end

--- client/src/Form/Form.js
+++ client/src/Form/Form.js
@@ -17,7 +17,10 @@ class Form extends Component {
       }
     
       handleSubmit(event) {
-        alert('An essay was submitted: ' + this.state.value);
+        //alert('An essay was submitted: ' + this.state.value);
+        postData('/api/summarize', {text: this.state.value})
+        .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+        .catch(error => console.error(error));
         event.preventDefault();
       }
     
@@ -32,6 +35,24 @@ class Form extends Component {
           </form>
         );
       }
+}
+
+function postData(url = ``, data = {}) {
+  // Default options are marked with *
+    return fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+    .then(response => response.json()); // parses response to JSON
 }
 
 export default Form;
