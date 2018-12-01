309704f3382f22e693e226ffd9a4e528533f5e65
Ksenia Koldaeva <kseniacold@gmail.com>
2018-12-01 11:08:38 -0800

    Default app code

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {users: [] }
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({users})
-    );
-  }
-
   render() {
     return (
       <div className="App">
-        <h1>Users</h1>
-        <ul>{this.state.users.map(user => 
-          <li key={user.id}>{user.username}</li>)}</ul>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
