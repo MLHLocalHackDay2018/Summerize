e494f2d5f9dbeda6feb3a8402adaf9a08bc1c7f2
Bryce <antiteal@gmail.com>
2018-12-01 18:44:35 -0800

    remove unused code and packages

--- client/src/Response/Response.js
+++ client/src/Response/Response.js
@@ -7,7 +7,6 @@ class Response extends Component {
         
     }
     render() {
-        // let result = JSON.parse(this.props);
         console.log('from response component render:', this.props);
         console.log('from response component render:', this.props.result.summary);
 
@@ -44,23 +43,13 @@ class Response extends Component {
 }
 
 function isEmpty(obj) {
-
-    // null and undefined are "empty"
     if (obj == null) return true;
 
-    // Assume if it has a length property with a non-zero value
-    // that that property is correct.
     if (obj.length > 0)    return false;
     if (obj.length === 0)  return true;
 
-    // If it isn't an object at this point
-    // it is empty, but it can't be anything *but* empty
-    // Is it empty?  Depends on your application.
     if (typeof obj !== "object") return true;
 
-    // Otherwise, does it have any properties of its own?
-    // Note that this doesn't handle
-    // toString and valueOf enumeration bugs in IE < 9
     for (var key in obj) {
         if (hasOwnProperty.call(obj, key)) return false;
     }
